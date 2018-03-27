0b714967cb16013d1cdbeb308ff327a7a13a01b1
nex3z <litianxing9@gmail.com>
2018-03-27 21:02:03 +0800

    Add getTextView() to retrieve the built-in TextView.

--- togglebuttongroup/src/main/java/com/nex3z/togglebuttongroup/button/MarkerButton.java
+++ togglebuttongroup/src/main/java/com/nex3z/togglebuttongroup/button/MarkerButton.java
@@ -6,7 +6,6 @@ import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.widget.ImageView;
@@ -132,6 +131,10 @@ public abstract class MarkerButton extends CompoundToggleButton {
 
     public void setMarkerColor(int markerColor) {
         mMarkerColor = markerColor;
+    }
+
+    public TextView getTextView() {
+        return mTvText;
     }
 
     protected int getDefaultTextColor() {
